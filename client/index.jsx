7314f53e9e53c58f103e671faa8ac693b05dff65
Elderen Lee <elderen2@gmail.com>
2019-07-13 21:19:21 -0400

    prototype

--- client/index.jsx
+++ client/index.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SpotifyPlayer from 'react-spotify-player';
+
+const size = {
+  width: '45%',
+  height: 100,
+};
+const view = 'coverart'; // or 'coverart'
+const theme = 'black'; // or 'white'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: null
+      uri: ["spotify:track:2XeZi7qwChzLOt32tsoPXV", "spotify:track:4PouRT4KlI9TRHp45uMqdO", "spotify:track:1D2L6MefbXon28PzIk9I3r", "spotify:track:7LnU4vflcZ8fyuFaVxGtuN"]
     }
   }
   render() {
     return (
-      // <Fragment>
-        <div>
-          <p>hi</p>
+      <>
+        <div className="navbar">
+          <p id="nv-about"> About </p>
+          <p id="nv-video"> Video </p>
+          <p id="nv-music"> Music </p>
+          <p id="nv-contact"> Contact </p>
         </div>
-      // </Fragment>
+        <div id="filler" />
+        <div className="spotify">
+          {this.state.uri.map((id) => {
+            return <SpotifyPlayer
+              key={id}
+              uri={id}
+              size={size}
+              view={view}
+              theme={theme}
+            />
+          })}
+        </div>
+      </>
     )
   }
 }
